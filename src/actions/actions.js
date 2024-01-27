b97f65d35be7feb31757b5d6214d602fafe1ed06
adrian <adrian@google.com>
2024-01-27 19:03:48 -0400

    creando el server action de prueba

---
+++ src/actions/actions.js
@@ -0,0 +1,28 @@
+'use server';
+import { Client } from 'pg'
+import { cookies } from 'next/headers';
+
+
+export const ProcessDatabaseQuery = () => {
+    const cookieStore = cookies();
+    const userName = cookieStore.get('userName')?.value ?? 'postgres';
+    const password = cookieStore.get('password')?.value ?? '8066';
+
+    const db_user = new Client({
+        user: userName,
+        host: 'localhost',
+        database: 'restaurante', // restaurante
+        password: password,
+        port: 4321,
+    });
+    try {
+        db_user.connect();
+        const query = `
+        SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';
+        `;
+        const queryResponse = db_user.query(query);
+        return queryResponse.rows;
+    } catch (error) {
+        console.log(error);
+    }
+}

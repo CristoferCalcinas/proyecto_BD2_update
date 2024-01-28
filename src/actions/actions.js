1a58b9eec1ad53b0a895e2493e001ef5486b91ac
adrian <adrian@google.com>
2024-01-28 09:57:09 -0400

    separando los server actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,34 @@
 'use server';
 import { Client } from 'pg'
 import { cookies } from 'next/headers';
+// import { revalidatePath } from 'next/cache';
 
 
-export const ProcessDatabaseQuery = async () => {
+export const ProcessDatabaseQuery = async (query) => {
+    const cookieStore = cookies();
+    const userName = cookieStore.get('userName')?.value ?? 'postgres';
+    const password = cookieStore.get('password')?.value ?? '8066';
+
+    const db_user = new Client({
+        user: userName,
+        host: 'localhost',
+        database: 'restaurante', // restaurante
+        password: password,
+        port: 4321,
+    });
+    try {
+        db_user.connect();
+        const queryResponse = await db_user.query(query);
+        db_user.end();
+        return queryResponse.rows;
+    } catch (error) {
+        console.log(error);
+    }
+    // revalidatePath('/');
+};
+
+
+export const WatchDatabaseTables = async () => {
     const cookieStore = cookies();
     const userName = cookieStore.get('userName')?.value ?? 'postgres';
     const password = cookieStore.get('password')?.value ?? '8066';
@@ -21,10 +46,10 @@ export const ProcessDatabaseQuery = async () => {
         SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';
         `;
         const queryResponse = await db_user.query(query);
+        db_user.end();
         return queryResponse.rows;
     } catch (error) {
         console.log(error);
     }
-};
-
-
+    // revalidatePath('/');
+}

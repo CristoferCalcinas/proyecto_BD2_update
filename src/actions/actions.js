15e0b5209bb933f194f3e6376be4c35f7b3313f5
adrian <adrian@google.com>
2024-01-28 11:15:04 -0400

    agregando la funcion de click y hacer un select en las tablas del sidebar

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,7 +3,7 @@ import { Client } from 'pg'
 import { cookies } from 'next/headers';
 // import { revalidatePath } from 'next/cache';
 
-
+// Se envia una query desde el textarea, esta funcion la procesa y se retorna el resultado
 export const ProcessDatabaseQuery = async (query) => {
     const cookieStore = cookies();
     const userName = cookieStore.get('userName')?.value ?? 'postgres';
@@ -27,7 +27,7 @@ export const ProcessDatabaseQuery = async (query) => {
     // revalidatePath('/');
 };
 
-
+// Ejecuta una query para retornar las tablas dentro de la base de datos
 export const WatchDatabaseTables = async () => {
     const cookieStore = cookies();
     const userName = cookieStore.get('userName')?.value ?? 'postgres';
@@ -53,3 +53,30 @@ export const WatchDatabaseTables = async () => {
     }
     // revalidatePath('/');
 }
+
+// Ejecuta una query de tipo SELECT para retornar la informacion de una tabla
+export const WatchTableData = async (tableName) => {
+    const cookieStore = cookies();
+    const userName = cookieStore.get('userName')?.value ?? 'postgres';
+    const password = cookieStore.get('password')?.value ?? '8066';
+
+    const db_user = new Client({
+        user: userName,
+        host: 'localhost',
+        database: 'restaurante', // restaurante
+        password: password,
+        port: 4321,
+    });
+    try {
+        db_user.connect();
+        const query = `
+        SELECT * FROM ${tableName};
+        `;
+        const queryResponse = await db_user.query(query);
+        db_user.end();
+        return queryResponse.rows;
+    } catch (error) {
+        console.log(error);
+    }
+    // revalidatePath('/');
+} 
--- src/components/sidebar/components/DatabaseTablesListener.jsx
+++ src/components/sidebar/components/DatabaseTablesListener.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
-import { WatchDatabaseTables } from "@/actions/actions";
+import { WatchDatabaseTables, WatchTableData } from "@/actions/actions";
 
 export const DatabaseTablesListener = () => {
   const [tablesList, setTablesList] = useState([]);
@@ -27,22 +27,32 @@ export const DatabaseTablesListener = () => {
 };
 
 const TablesListenerList = ({ tablename }) => {
+  const handleDescribeTable = async (tablename) => {
+    console.log(tablename);
+    const respSELECT = await WatchTableData(tablename);
+    console.log(respSELECT);
+  };
   return (
-    <div className="min-w-full py-2 align-middle pl-7 pr-4 flex items-center justify-around">
-      <table className="min-w-full divide-y divide-gray-300">
-        <thead>
-          <tr>
-            <th
-              scope="col"
-              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-0 text-white"
-            >
-              {tablename}
-            </th>
-          </tr>
-        </thead>
-      </table>
-      <div>
-        <EllipsisVerticalIcon className="h-6 w-6 text-white" />
+    <div className="min-w-full py-2 align-middle pl-7 pr-4 flex items-center justify-between">
+      <div
+        onClick={() => handleDescribeTable(tablename)}
+        className="cursor-pointer"
+      >
+        <table className="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-0 text-white"
+              >
+                {tablename}
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="bg-red-500 h-full">
+        <EllipsisVerticalIcon className="h-10 w-6 text-white" />
       </div>
     </div>
   );

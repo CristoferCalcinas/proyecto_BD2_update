2a561547889cc77f39e4d360c8b8593823fc70f6
adrian <adrian@google.com>
2024-01-28 10:40:33 -0400

    separando las tablas y agregando nuevo funcionalcomponent

--- src/components/sidebar/components/DatabaseTablesListener.jsx
+++ src/components/sidebar/components/DatabaseTablesListener.jsx
@@ -14,13 +14,32 @@ export const DatabaseTablesListener = () => {
 
   return (
     <div>
-      <ul className="space-y-1">
-        {tablesList.map((item, index) => (
+      <ul className="flex flex-col items-center">
+        {tablesList.map(({ tablename }, index) => (
           <li key={index}>
-            <div>{item.tablename}</div>
+            <TablesListenerList tablename={tablename} />
           </li>
         ))}
       </ul>
+    </div>
+  );
+};
+
+const TablesListenerList = ({ tablename }) => {
+  return (
+    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+      <table className="min-w-full divide-y divide-gray-300">
+        <thead>
+          <tr>
+            <th
+              scope="col"
+              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-0 text-white"
+            >
+              {tablename}
+            </th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 };

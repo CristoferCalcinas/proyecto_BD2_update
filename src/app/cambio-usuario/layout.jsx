14811f6f45a9a565d67845a481c8c21427b59d0d
adrian <adrian@google.com>
2024-01-29 12:27:40 -0400

    implementacion de la metadata

---
+++ src/app/cambio-usuario/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Cambio de Usuario",
+  description: "Cambio de Usuario para la administración de una base de datos",
+};
+
+export default function CambioUsuarioPageLayout({ children }) {
+  return <div>{children}</div>;
+}

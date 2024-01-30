34b1b8ae6d8a7e256c044d74f7a325e4a4279ac2
adrian <adrian@google.com>
2024-01-30 14:33:34 -0400

    probando el display grid

--- src/components/sidebar/ContentArea.jsx
+++ src/components/sidebar/ContentArea.jsx
@@ -5,7 +5,7 @@ import { UserDisplayName } from "../userSwitcher/UserDisplayName";
 
 export const ContentArea = async ({ children }) => {
   return (
-    <div className="lg:pl-72">
+    <section id="ContentArea">
       <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
         <button
           type="button"
@@ -49,6 +49,6 @@ export const ContentArea = async ({ children }) => {
       <main className="py-10">
         <div className="px-4 sm:px-6 lg:px-8">{children}</div>
       </main>
-    </div>
+    </section>
   );
 };
